b2c374cd979945cabf6daa466efc7c2966d984dc
Светлана Рыкова <srv8@mail.ru>
2022-08-24 12:02:28 +0300

    changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,7 @@ public class Main {
         post.patronymic = "Иванович";
         post.phone = "+7 (999) 999-99-99";
         post.surname = "Иванов";
-        post.subscription = "true";
+        post.subscription = true;
 
         post.birthday = new FormDate();
         post.birthday.day = 13;
